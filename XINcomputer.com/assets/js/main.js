f174ee615322f31b165c760d885f762d84e2cfb6
Lê Văn Lương <125948228+Luongutk89@users.noreply.github.com>
2023-02-23 16:02:19 +0700

    XINcomputer

---
+++ XINcomputer.com/assets/js/main.js
@@ -0,0 +1,135 @@
+$(document).ready(function() {
+    $('#main-featured').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 5.17
+            },
+            1366: {
+                items: 6.17
+            }
+        },
+    });
+    $('#main-products').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3.6
+            },
+            1366: {
+                items: 4.6
+            }
+        },
+    });
+
+    $('#main-products-2').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3.6
+            },
+            1366: {
+                items: 4.6
+            }
+        },
+    });
+    $('#main-products-3').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3.6
+            },
+            1366: {
+                items: 4.6
+            }
+        },
+    });
+    $('#main-products-4').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3.6
+            },
+            1366: {
+                items: 4.6
+            }
+        },
+    });
+    $('#main-products-5').owlCarousel({
+        loop: true,
+        margin: 10,
+        nav: true,
+        autoplay: true,
+        autoplayTimeout: 3500,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3.6
+            },
+            1366: {
+                items: 4.6
+            }
+        },
+    });
+});
